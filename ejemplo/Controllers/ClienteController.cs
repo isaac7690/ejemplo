3e73997a9f6ba03ec1286468e18b0f1711b2a7fc
Isaac <isaac769@hotmail.com>
2023-07-21 22:54:24 -0500

    controlador cliente creado

---
+++ ejemplo/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ejemplo.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
